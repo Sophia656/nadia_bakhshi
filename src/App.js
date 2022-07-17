4e51ad896ab46f79357f1bf1d12f21cf38e0f75c
sophia656 <antropovasophia@gmail.com>
2022-07-17 15:30:16 +0300

    video format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import videoBack from './video/My Movie 19.mp4';
+import videoBack2 from './video/My Movie 19.webm';
+import videoBack3 from './video/My Movie 19.mov';
 import videoTab from './video/vertical tab.mp4';
+import videoTab2 from './video/vertical tab.webm';
+import videoTab3 from './video/vertical tab.mov';
 import videoMob from './video/mob.mp4';
+import videoMob2 from './video/mob.webm';
+import videoMob3 from './video/mob.mov';
 import './index.css';
 
 function getWindowDimensions() {
@@ -15,15 +21,23 @@ function getWindowDimensions() {
 function App() {
   const width = getWindowDimensions()
 
-
-
-  console.log('width', width)
-
   return (
     <div className="wrapper">
-      {width.width > 820 && <video src={videoBack} style={{width: '100%', height: '100%', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted />}
-      {(width.width < 821 && width.width > 426) && <video src={videoTab} style={{width: 'auto', height: '100vh', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted />}
-      {width.width < 426 && <video src={videoMob} style={{width: 'auto', height: '100vh', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted />}
+      {width.width > 820 && <video src={videoBack} style={{width: '100%', height: '100%', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted >
+        <source src={videoBack} type="video/mp4"></source>
+        <source src={videoBack2} type="video/webm"></source>
+        <source src={videoBack3} type="video/mov"></source>
+      </video>}
+      {(width.width < 821 && width.width > 426) && <video src={videoTab} style={{width: '100%', height: '100%', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted >
+              <source src={videoTab} type="video/mp4"></source>
+              <source src={videoTab2} type="video/webm"></source>
+              <source src={videoTab3} type="video/mov"></source>
+      </video>}
+      {width.width < 426 && <video src={videoMob} style={{width: '100%', height: '100%', top: 0, right: 0, bottom: 0, letf: 0}} autoPlay playsInline loop muted >
+              <source src={videoMob} type="video/mp4"></source>
+              <source src={videoMob2} type="video/webm"></source>
+              <source src={videoMob3} type="video/mov"></source>
+      </video>}
     </div>
   );
 }
